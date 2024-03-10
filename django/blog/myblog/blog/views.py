467f9801ab1f22b1f1f93c47230f6b00205787f8
Akshay Bodla <akshaybodla21@gmail.com>
2024-03-10 09:16:58 -0700

    view for creating form

--- django/blog/myblog/blog/views.py
+++ django/blog/myblog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -8,3 +9,13 @@ def post_list(request):
         {'title': 'My first blog', 'body': 'Bite my shiny metal ass!'}
     ]
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
