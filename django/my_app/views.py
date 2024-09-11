ab962aa270ddbfe6af7a1b7e734f30e8ad974c71
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-10 18:41:30 -0700

    dynamic url

--- django/my_app/views.py
+++ django/my_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(req):
     return HttpResponse("Hello World")
+
+def about(req):
+    return HttpResponse("<h1>About</h1>")
+
+def hello(req, name):
+    return HttpResponse(f"Hello {name}")
