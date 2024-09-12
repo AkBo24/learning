68652c933d2003b34eab0f32702cfb0a680882bc
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-12 13:38:46 -0700

    created base html for job board

---
+++ django/learning-app/my_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(req):
+    return HttpResponse("Hello World")
+
+def about(req):
+    return HttpResponse("<h1>About</h1>")
+
+def hello(req, name):
+    return HttpResponse(f"Hello {name}")
+
+def add(req, x, y):
+    return HttpResponse(f"res: {x+y}")
