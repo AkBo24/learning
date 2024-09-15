263db5a2e180523cbaa5cc810c5213b598ead42a
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-15 12:57:49 -0700

    user auth

--- django/trip-talk/trip/views.py
+++ django/trip-talk/trip/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 from . import models
 
@@ -8,8 +10,13 @@ class HomeView(TemplateView):
     template_name = "trip/index.html"
 
 def trips_list(req):
-    trips = models.Trip.objects.all()
+    trips = models.Trip.objects.filter(owner=req.user)
     context = {
         'trips': trips
     }
     return render(req, 'trip/trips_list.html', context)
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
