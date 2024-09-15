7b070ae3b97c82b309576d64333db0c6fff20b58
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-15 13:59:12 -0700

    styling + create trip view

--- django/trip-talk/trip/urls.py
+++ django/trip-talk/trip/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('dashboard/', views.trips_list, name='trips-list')
+    path('dashboard/', views.trips_list, name='trips-list'),
+    path('dashboard/trip/create/', views.TripCreateView.as_view(), name='trip-create'),
+    path('dashboard/trip/<int:pk>/', views.trips_list, name='trip-detail'),
+    path('', views.trips_list, name='note-list'),
+    path('', views.trips_list, name='note-create'),
 ]
--- django/trip-talk/trip/views.py
+++ django/trip-talk/trip/views.py
@@ -20,3 +20,13 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+class TripCreateView(CreateView):
+    model = models.Trip
+    fields = ["city", "country", "start_date", "end_date"]
+    success_url = reverse_lazy('trips-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
