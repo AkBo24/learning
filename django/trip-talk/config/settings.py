6e09b8b4b6a150987f4ed133004ff8c6132aca95
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-15 12:47:25 -0700

    user auth

--- django/trip-talk/config/settings.py
+++ django/trip-talk/config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+LOGIN_REDIRECT_URL = 'trips-list'
--- django/trip-talk/config/urls.py
+++ django/trip-talk/config/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('trip.urls'))
+    path('', include('trip.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 
