dc48f7009bfa51f8d7bb0961d9f733e3058af566
Akshay Bodla <akshaybodla21@gmail.com>
2024-09-16 10:46:14 -0700

    Note detail + add views

--- django/trip-talk/trip/views.py
+++ django/trip-talk/trip/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, DetailView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 
@@ -29,4 +29,35 @@ class TripCreateView(CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+    
+class TripDetailView(DetailView):
+    model = models.Trip
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        trip = context['object']
+        notes = trip.notes.all()
+        context['notes'] = notes
+        return context
+
+
+class NoteCreateView(CreateView):
+    model = models.Note
+    fields = ["name", "description", "type", "rating", "trip"]
+    success_url = reverse_lazy('note-list')
+
+    def get_form(self):
+        form = super(NoteCreateView, self).get_form()
+        print(form.fields)
+        form.fields['trip'].queryset = models.Trip.objects.filter(owner=self.request.user)
+        return form
+
+class NoteDetailView(DetailView):
+    model = models.Note
+
+class NoteListView(ListView):
+    model = models.Note
+
+    def get_queryset(self):
+        return models.Note.objects.filter(trip__owner=self.request.user)
+    
