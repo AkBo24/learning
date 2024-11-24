04c92e22723d326a414dc326810c5e744d890af5
Akshay Bodla <akshaybodla21@gmail.com>
2024-11-24 12:08:18 -0800

    created todo api

--- fullstack/todo-app/todo/views.py
+++ fullstack/todo-app/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+
+from . import models
+from . import serializers
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = serializers.TodoSerializer
