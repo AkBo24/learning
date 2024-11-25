44d18e504af73be4fc8e707174ef4a598f0a48d9
Akshay Bodla <akshaybodla21@gmail.com>
2024-11-25 11:43:43 -0800

    Added inputs for title and description

--- fullstack/todo-app/frontend/src/App.tsx
+++ fullstack/todo-app/frontend/src/App.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import Todo from './components/Todo';
+import React, { useState } from 'react';
 import { useAddTodoMutation, useGetTodosQuery } from './api/apiSlice';
+import Todo from './components/Todo';
 
 const App = () => {
     const { data: todos } = useGetTodosQuery(undefined);
     const [addTodo, result] = useAddTodoMutation();
 
-    console.log(todos);
+    const [todoTitle, setTodoTitle] = useState<string>('');
+    const [todoDescription, setTodoDescription] = useState<string>('');
 
     const handleAddTodo = async () => {
+        if (!todoTitle) {
+            alert('Enter todo title');
+            return;
+        }
         const newTodo: Todo = {
-            title: 'Manually added todo',
-            description: 'using a button to create this todo',
+            title: todoTitle,
+            description: todoDescription,
             completed: false,
         };
-        const t = await addTodo(newTodo);
-        console.log(t);
+        await addTodo(newTodo);
     };
 
     return (
         <div>
             <h1>Todos</h1>
-            <button onClick={handleAddTodo}>Add todo</button>
+
+            <form onSubmit={handleAddTodo}>
+                <div>
+                    <label htmlFor='new-todo-title'>Title</label>
+                    <input
+                        onChange={(e) => setTodoTitle(e.target.value)}
+                        id='new-todo-title'
+                        value={todoTitle}
+                        placeholder='Enter new todo…'
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor='new-todo-description'>Description</label>
+                    <textarea
+                        onChange={(e) => setTodoDescription(e.target.value)}
+                        id='new-todo-description'
+                        value={todoDescription}
+                        placeholder='Enter todo details'
+                    />
+                </div>
+                <button type='submit'>Add todo</button>
+            </form>
 
             {todos?.map((todo, i) => (
                 <ul key={i}>
