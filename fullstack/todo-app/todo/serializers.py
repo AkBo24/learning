a34db805ee97260064bc0b924cbc8ecfedc87d82
Akshay Bodla <akshaybodla21@gmail.com>
2025-01-01 20:04:50 -0800

    created deep dive dir for...deep dives into tools

--- fullstack/todo-app/todo/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Todo
-        fields = ["id", "title", "description", "completed"]
-        read_only_fields = ['id', 'created_at', 'updated_at', 'owner']
